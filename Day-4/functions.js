5b509f7180237eefd1baa512fe9aea495c88d6d6
Manikandan Anbalagan <manikandana121290@gmail.com>
2025-01-24 19:01:37 +0530

    Day 3 and 4

---
+++ Day-4/functions.js
@@ -0,0 +1,6 @@
+// displayName("Manikandan Anbalagan"); // Actual value - Argument - function call
+// displayName("Thiwan M");
+function displayName(name) {
+  // Parameter - name
+  console.log(name);
+}
